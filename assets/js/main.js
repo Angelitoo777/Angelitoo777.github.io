6b99fef71edc20071eb388cb5e576158084b321e
Angelitoo777 <angeloropeza1604@gmail.com>
2025-09-18 15:18:47 -0400

    ready yes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const reveals = document.querySelectorAll(".reveal");
-  const observer = new IntersectionObserver(entries => {
+  const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-      if(entry.isIntersecting){
+      if (entry.isIntersecting) {
         entry.target.classList.add("visible");
-        observer.unobserve(entry.target);
       }
     });
-  },{threshold:0.1});
+  }, { threshold: 0.1 });
+
   reveals.forEach(r => observer.observe(r));
 });
